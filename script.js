73d1880da0260ee7facffd443eab4051eacb78a2
Khalid Hussain <140644420+mkhalidh@users.noreply.github.com>
2024-02-26 12:50:42 +0500

    Files uploaded!

---
+++ script.js
@@ -0,0 +1,24 @@
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".part-2",
+    // markers:true,
+    start: "20% 50%",
+    end: "100% 59%",
+    scrub: 2,
+    pin: true,
+  },
+});
+tl.to(
+  "#image-anim-2",
+  {
+    opacity: 0,
+  },
+  "a"
+).to(
+  ".image-anim",
+  {
+    rotateX: "0deg",
+    top: "-10vh",
+  },
+  "a"
+);
